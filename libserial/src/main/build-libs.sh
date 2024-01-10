f4637f15541e4b4b96bfaecc3bebcaf101a9bf22
forkyguo <13811831379@qq.com>
2024-01-10 20:16:23 +0800

    gf add so

---
+++ libserial/src/main/build-libs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [[ -z "${ANDROID_NDK_HOME}" ]]; then
+  echo "ANDROID_NDK_HOME must be set! android-ndk-r21e is confirmed to work, but other version can also work."
+  echo "get it from here: https://developer.android.com/ndk/downloads"
+  exit 1
+fi
+
+ARGS=$(getopt -o "dca" -l "debug,clean,archive" -n "build-libs.sh" -- "$@");
+
+eval set -- "$ARGS";
+
+#cd libserial/src/main
+
+JNI_ARGS="NDK_LIBS_OUT=jniLibs"
+BUILD_TASK="assembleRelease"
+
+EXPORT_AAR=1
+
+while true; do
+  case "$1" in
+    -d|--debug)
+      shift
+      JNI_ARGS="$JNI_ARGS NDK_DEBUG=1"
+      BUILD_TASK="assembleDebug"
+      break;
+      ;;
+    -c|--clean)
+      shift
+      JNI_ARGS="$JNI_ARGS clean"
+      break;
+      ;;
+    -a|--archive)
+      shift
+      EXPORT_AAR=1
+      break;
+      ;;
+    --)
+      shift
+      break;
+      ;;
+  esac
+done
+
+# Remove '--'
+shift
+
+ndk-build $JNI_ARGS  APP_BUILD_SCRIPT=/Users/fengguo/Downloads/serial-android-master/libserial/src/main/jni/Android.mk NDK_APPLICATION_MK=/Users/fengguo/Downloads/serial-android-master/libserial/src/main/jni/Application.mk
+
+
+cd ../../..
+
+if [ "$EXPORT_AAR" = "1" ]; then
+  cd libserial
+  ../gradlew $BUILD_TASK
+  cd ..
+  echo "Done, output is at libserial/build/outputs/aar"
+fi
